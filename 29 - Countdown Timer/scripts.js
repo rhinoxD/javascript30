81307477e1d962401d6a92ca19a6391f1abf94f6
Shivam Kumar <coolshiv0721@gmail.com>
2023-02-06 19:28:04 +0530

    Countdown timer

---
+++ 29 - Countdown Timer/scripts.js
@@ -0,0 +1,69 @@
+let countdown
+const timerDisplay = document.querySelector('.display__time-left')
+const endTime = document.querySelector('.display__end-time')
+const buttons = document.querySelectorAll('[data-time]')
+
+const timer = (seconds) => {
+  // clear any existing timer
+  clearInterval(countdown)
+
+  const now = Date.now()
+  const then = now + seconds * 1000
+
+  displayTimeLeft(seconds)
+  displayEndTime(then)
+
+  countdown = setInterval(() => {
+    const secondsLeft = Math.round((then - Date.now()) / 1000)
+
+    // check if we should stop it!
+    if (secondsLeft < 0) {
+      clearInterval(countdown)
+      return
+    }
+
+    // display it
+    displayTimeLeft(secondsLeft)
+  }, 1000)
+}
+
+const displayTimeLeft = (seconds) => {
+  const minutes = Math.floor(seconds / 60)
+  const remainderSeconds = seconds % 60
+  const display = `${minutes}:${
+    remainderSeconds < 10 ? '0' : ''
+  }${remainderSeconds}`
+
+  document.title = display
+  timerDisplay.textContent = display
+}
+
+const displayEndTime = (timestamp) => {
+  const end = new Date(timestamp)
+  const hour = end.getHours()
+  const minutes = end.getMinutes()
+
+  endTime.textContent = `Be Back At ${hour}:${
+    minutes < 10 ? '0' : ''
+  }${minutes}`
+  // endTime.textContent = `Be Back At ${hour > 12 ? hour - 12 : hour}:${
+  //   minutes < 10 ? '0' : ''
+  // }${minutes}`
+}
+
+function startTimer() {
+  const seconds = +this.dataset.time
+  timer(seconds)
+}
+
+function customTime(e) {
+  e.preventDefault()
+
+  const mins = this.minutes.value
+  timer(mins * 60)
+  this.reset()
+}
+
+buttons.forEach((button) => button.addEventListener('click', startTimer))
+
+document.customForm.addEventListener('submit', customTime)
